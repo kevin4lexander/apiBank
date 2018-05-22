534b2605c8bc7a5d38e5866dffc7efe3fb7d596e
kevin4lexander <kevinamh24@gmail.com>
2018-05-21 19:56:58 -0500

    Update ApiBank

--- index.js
+++ index.js
@@ -3,15 +3,15 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
-mongoose.connect("mongodb://localhost/mydb", (err, res) => {
-	if (err) {
-		return console.log("Error conecting to DB: " + err)
-	}
-	console.log("Conected to DB...")
+mongoose.connect("mongodb://easyfinance:easyf2018@ds129560.mlab.com:29560/apibank", (err, res) => {
+    if (err) {
+        return console.log("Error conecting to DB: " + err)
+    }
+    console.log("Conected to DB...")
 
-	app.listen(port, () => {
-		console.log("API running on http://localhost:" + port)
-	})
+    app.listen(port, () => {
+        console.log("API running on http://localhost:" + port)
+    })
 })
 const port = process.env.PORT || 3001
 
@@ -20,7 +20,6 @@ const app = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
-
 
 
 // POST
@@ -44,84 +43,126 @@ app.post("/api-bank/user", (req, res) => {
     })
 })
 
+
 // GET
 
 // all user
 app.get("/api-bank/user", (req, res) => {
-	User.find({}, (err, users) => {
-		if(err) return res.status(500).send({message: "Error with the request "+ err})
-		if(!users) return res.status(404).send({message: "Users not found"})
+    User.find({}, (err, users) => {
+        if(err) return res.status(500).send({message: "Error with the request "+ err})
+        if(!users || users.length==0) return res.status(404).send({message: "Empty"})
 
-		res.send(200, {users})
-	})
+        res.send(200, {users})
+    })
 })
 
-// accounts only one user
+// accounts of only one user
 app.get("/api-bank/user/:idUser", (req, res) => {
-	let idUser = req.params.idUser
+    let idUser = req.params.idUser
 
-	User.find({id: idUser}, (err, user) => {
-		if(err) return res.status(500).send({message: "Error with the request "+ err})
-		if(!user || user.length==0) return res.status(404).send({message: "User not found"})
-
-		res.status(200).send({user})
-	})
+    User.find({id: idUser}, (err, accounts) => {
+        if(err) return res.status(500).send({message: "Error with the request "+ err})
+        if(!accounts || accounts.length==0) return res.status(404).send({message: "Account not found"})
+accounts
+        res.status(200).send({accounts})
+    })
 })
 
-// specific account
+// cuentas de un banco, tipo y usuario especificos
+app.get("/api-bank/typeAcc/:bank/:typeAcc/:idUser", (req, res) => {
+    let idUser = req.params.idUser
+    let bank = req.params.bank
+    let typeAcc = req.params.typeAcc
+
+    User.find({id: idUser, bankAcc: bank, typeAcc: typeAcc}, (err, account) => {
+        if(err) return res.status(500).send({message: "Error with the request "+ err})
+        if(!account || account.length==0) return res.status(404).send({message: "Account not found"})
+
+        res.status(200).send({account})
+    })
+})
+
+// cuentas de una banco y usuario especificos
+app.get("/api-bank/bankAcc/:bank/:idUser", (req, res) => {
+    let idUser = req.params.idUser
+    let bank = req.params.bank
+
+    User.find({id: idUser, bankAcc: bank}, (err, account) => {
+        if(err) return res.status(500).send({message: "Error with the request "+ err})
+        if(!account || account.length==0) return res.status(404).send({message: "Account not found"})
+
+        res.status(200).send({account})
+    })
+})
+
+// cuenta especifica de un usuario
 app.get("/api-bank/user/:idUser/:numberAcc", (req, res) => {
-	let idUser = req.params.idUser
-	let numberAcc = req.params.numberAcc
+    let idUser = req.params.idUser
+    let numberAcc = req.params.numberAcc
 
-	User.findOne({id: idUser, numberAcc: numberAcc}, (err, user) => {
-		if(err) return res.status(500).send({message: "Error with the request "+ err})
-		if(!user || user.length==0) return res.status(404).send({message: "User not found"})
+    User.findOne({id: idUser, numberAcc: numberAcc}, (err, account) => {
+        if(err) return res.status(500).send({message: "Error with the request "+ err})
+        if(!account || account.length==0) return res.status(404).send({message: "User not found"})
 
-		res.status(200).send({user})
-	})
+        res.status(200).send({account})
+    })
 })
 
+// verificar numero de cuenta y clave de una cuenta bancaria de usuario
+app.get("/api-bank/:idUser/:numberAcc/:key", (req, res) => {
+    let idUser = req.params.idUser
+    let numberAcc = req.params.numberAcc
+    let key = req.params.key
+
+    User.findOne({id: idUser, numberAcc: numberAcc, keyAcc: key}, {numberAcc}, (err, account) => {
+        if(err) return res.status(500).send({message: "Error with the request "+ err})
+        if(!account || account.length==0) return res.status(404).send({message: "User not found"})
+
+        res.status(200).send({account})
+    })
+})
 
 // PUT
 
 // update user data
 app.put("/api-bank/user/:idUser", (req, res) => {
-	let idUser = req.params.idUser
-	let body = req.body
+    let idUser = req.params.idUser
+    let body = req.body
 
-	User.findByIdAndUpdate(idUser, body, (err, updatedUser) => {
-		if(err) res.status(500).send({message: "Error user not update: "+ err})
+    User.findByIdAndUpdate(idUser, body, (err, updatedUser) => {
+        if(err) res.status(500).send({message: "Error user not update: "+ err})
 
-		res.status(200).send({user: updatedUser})
-	})
+        res.status(200).send({user: updatedUser})
+    })
 })
 
-//update balance
+//update balance of account
 app.put("/api-bank/user/:idUser/:numberAcc", (req, res) => {
-	let idUser = req.params.idUser
-	let numberAcc = req.params.numberAcc
-	let balance = req.body.balanceAcc
-	//let body = req.body
+    let idUser = req.params.idUser
+    let numberAcc = req.params.numberAcc
+    let balance = req.body.balanceAcc
+    //let body = req.body
 
-	User.findOneAndUpdate({id: idUser, numberAcc: numberAcc}, {balanceAcc: balance}, (err, user) => {
-		if(err) res.status(500).send({message: "Error updating account balance "+ err})
+    User.findOneAndUpdate({id: idUser, numberAcc: numberAcc}, {balanceAcc: balance}, (err, user) => {
+        if(err) res.status(500).send({message: "Error updating account balance "+ err})
         
         res.status(200).send({user})
-	})
+    })
 })
+
 
 // DELETE
 
 //delete user
 app.delete("/api-bank/user/:idUser", (req, res) => {
-	let idUser = req.params.idUser
+    let idUser = req.params.idUser
 
-	User.findOne({id: idUser}, (err, user) => {
-		if(err) res.status(500).send({mesage: "Error user not deleted: "+ err})
+    User.findOne({id: idUser}, (err, user) => {
+        if(err) res.status(500).send({mesage: "Error user not deleted: "+ err})
 
-		user.remove(err => {
-			if(err) res.status(500).send({mesage: "Error user not deleted: "+ err})
-			res.status(200).send({message: "User deleted"})
-		})
-	})
+        user.remove(err => {
+            if(err) res.status(500).send({mesage: "Error user not deleted: "+ err})
+            res.status(200).send({message: "User deleted"})
+        })
+    })
 })
